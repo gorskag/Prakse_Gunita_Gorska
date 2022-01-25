03fbfc9ce5004d3df6de9bde77932169183ed912
GunitaG <gunitagorska@gmail.com>
2022-01-25 17:07:15 +0200

    commit on 'master'

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -8,3 +8,53 @@ function sparkleDetails() {
         x.style.display = "none";
     }
 }
+
+
+// --- On click slides to the previous/next card ---
+$(document).ready(function(){
+    $("#next").click(function(){
+        $("#card1").css({
+            "opacity":"0",
+            "display":"none",
+            }).hide().animate({opacity:1});
+      $("#card2").css({
+        "opacity":"0",
+        "display":"block",
+        }).show().animate({opacity:1});
+    });
+
+
+    $("#prev").click(function(){
+        $("#card1").css({
+            "opacity":"0",
+            "display":"block",
+            }).show().animate({opacity:1});
+      $("#card2").css({
+        "opacity":"0",
+        "display":"none",
+        }).hide().animate({opacity:1});
+    });
+
+    $("#next2").click(function(){
+        $("#card2").css({
+            "opacity":"0",
+            "display":"none",
+            }).hide().animate({opacity:1});
+      $("#card3").css({
+        "opacity":"0",
+        "display":"block",
+        }).show().animate({opacity:1});
+    });
+
+
+    $("#prev2").click(function(){
+        $("#card2").css({
+            "opacity":"0",
+            "display":"block",
+            }).show().animate({opacity:1});
+      $("#card3").css({
+        "opacity":"0",
+        "display":"none",
+        }).hide().animate({opacity:1});
+    });
+  });
